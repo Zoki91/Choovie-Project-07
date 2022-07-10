ee3194d16083f9769db63bf9f913365204a5ac37
OGray96 <ogray54@gmail.com>
2022-07-10 19:04:34 +1000

    styled my List page

--- Assets/movieGenerator.js
+++ Assets/movieGenerator.js
@@ -1,6 +1,6 @@
 
 var myMoviesArray = [];
-
+var details =[];
 var movieListEl = $('#myListPage')
 
 function init (){
@@ -15,14 +15,31 @@ function init (){
     for(var i = 0; i < myListMovies.length; i++){
         //creating a div for each movies details to be saved
         var movieDiv = $('<div>');
-        movieDiv.attr('id',i)
-        movieDiv.attr('class',"movie-div move-down")
+        movieDiv.attr('id', myListMovies[i].id)
+        movieDiv.attr('class',"movie-div-mylist move-down movies")
 
         //creating element to store movie title
         var movieTitleEl = $('<h3>');
         var movieTitle = myListMovies[i].name;
-        movieTitleEl.attr('class','title')
+        movieTitleEl.attr('class','title movie-title movie-child')
         movieTitleEl.text(movieTitle);
+
+        //creating a poster element
+        var posterEl = $('<img>');
+        posterEl.attr('class','poster');
+        posterEl.attr('data-poster',i);
+
+        //creating a year element
+        var yearEl = $('<p>');
+        yearEl.attr('class','year');
+        yearEl.attr('data-year',i);
+
+        //creating plot element
+        var plotEl = $('<p>');
+        plotEl.attr('class','plot');
+        plotEl.attr('data-plot',i)
+
+
 
         //creating button that will remove movie from list
         var deleteBtn = $('<button>');
@@ -31,9 +48,14 @@ function init (){
 
         //appending elements to the page
         movieListEl.append(movieDiv);
+        movieDiv.append(posterEl);
         movieDiv.append(movieTitleEl);
+        // movieDiv.append(plotEl);
+        // movieDiv.append(yearEl);
         movieDiv.append(deleteBtn);
+
     }}
+    movieDetailRequest();
 }
 
 init();
@@ -65,12 +87,58 @@ $(document).on('click','.delete-mylist-element', function(event){
     myListMovies.splice(deleteIndex,1);
     console.log(myListMovies)
     localStorage.setItem("Movie-Details",JSON.stringify(myListMovies));
-
-
-
-
-
-
-    
+ 
 })
+
+function movieDetailRequest(){
+    movies = $(".movies");
+    var ids = [];
+    movies.each(function(){
+        var movieIds = $(this).attr("id");
+        ids.push(movieIds)
+    })
+    console.log(ids)
+
+    for(i=0; i < ids.length; i++){
+        var plotUrl = 'https://www.omdbapi.com/?apikey=4cf0dfc5&i=' + ids[i];
+        console.log(plotUrl)
+        fetch(plotUrl)
+        .then(function (response) {
+            //storing data as an json object
+            return response.json();
+          })
+            .then(function(plotdata){
+                details.push(plotdata) ;       
+            })   
+ 
+    }
+
+    applyingDetailsRun = setTimeout(function(){
+        details.sort(function (a,b){
+            return ids.indexOf(a.imdbID) - ids.indexOf(b.imdbID)
+        })
+        for(x=0; x < details.length; x++){
+            var y = x;
+            stringX = y.toString();
+            //Adding Poster to elements
+            posterTarget = $('.poster[data-poster=' + stringX + ']')
+            posterTarget.attr('src', details[x].Poster)
+            posterTarget.attr("style","width:200px; height:300px; margin-left:-4px; margin-top: 80px")
+
+            plotTarget = $('.plot[data-plot=' + stringX + ']')
+            plotTarget.text(details[x].Plot)
+
+            yearTarget = $('.year[data-year=' + stringX + ']')
+            yearTarget.text(details[x].Year)
+            // dataTarget = $('.plotDescription[data-plot=' + stringX +']')
+            // imdbTarget = $('.imdbRating[data-imdb=' + stringX +']')
+            // boxOfficeTarget = $('.boxOffice[data-boxoffice=' + stringX +']')
+            // dataTarget.text(details[x].Plot)
+            // imdbTarget.text(details[x].imdbRating)
+            // boxOfficeTarget.text(details[x].BoxOffice)
+        }
+    },2000);
+
+
+}
 
